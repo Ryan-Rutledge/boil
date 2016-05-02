173fec720152eca3bca7c92bf2653cba8329a7eb
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2016-05-02 01:07:41 -0500

    Add c++ template

---
+++ plates/c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+{BP_FUNC_BEG}
+void {BP_FNAME}(){BP_LINE_BEG}
+	{BP_LINE_END}{
+	return;
+}
+{BP_FUNC_END}
+
+int main(){BP_LINE_BEG}
+	{BP_LINE_END}{
+	return 0;
+}
