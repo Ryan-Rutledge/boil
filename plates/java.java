d2127eaf957691fc953f7bce601b72d8de5ca498
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2016-05-02 01:07:55 -0500

    Add java template

---
+++ plates/java.java
@@ -0,0 +1,14 @@
+public class {BP_NAME}{BP_LINE_BEG}
+{BP_LINE_END}{
+	public static void main(String[], args){BP_LINE_BEG}
+	{BP_LINE_END}{
+		return;
+	}
+
+{BP_FUNC_BEG}
+	public static void {BP_FNAME}(){BP_LINE_BEG}
+	{BP_LINE_END}{
+		return;
+	}
+{BP_FUNC_END}
+}
