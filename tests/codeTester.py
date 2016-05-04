47efd2db7177a14491a3ce108d880f62eb4fea4c
Ryan-Rutledge <ryan.rutledge.dev@gmail.com>
2016-05-04 01:29:02 -0500

    Add unit tests for remaining languages

--- tests/codeTester.py
+++ tests/codeTester.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# r !boil -m green --title Blue -l python2 -n -s 2
+
 options = {
     'funcs'    : ['green'],
     'newlines' : True,
@@ -15,6 +17,81 @@ class Tester:
         self.ext      = ext
         self.default  = default
         self.advanced = advanced
+
+c = Tester('c', '.c',
+'''#include <stdio.h>
+
+int main() {
+	return 0;
+}
+''',
+'''#include <stdio.h>
+
+void green()
+{
+  return;
+}
+
+int main()
+{
+  return 0;
+}
+''')
+
+cpp = Tester('c++', '.cpp',
+'''#include <iostream>
+
+using namespace std;
+
+int main() {
+	return 0;
+}
+''',
+'''#include <iostream>
+
+using namespace std;
+
+void green()
+{
+  return;
+}
+
+int main()
+{
+  return 0;
+}
+''')
+
+html5 = Tester('html5', '.html',
+'''<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>DEFAULT_NAME</title>
+
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+
+	<body></body>
+</html>
+''',
+'''<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Blue</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+
+  <body>
+    <div id="green">
+    </div>
+  </body>
+</html>
+''')
 
 java = Tester('java', '.java',
 '''public class DEFAULT_NAME {
@@ -49,6 +126,48 @@ if __name__ == '__main__':
 ''',
 
 '''#!/usr/bin/env python
+
+def green():
+  pass
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
+''')
+
+python2 = Tester('python2', '.py',
+'''#!/usr/bin/env python2
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+''',
+'''#!/usr/bin/env python2
+
+def green():
+  pass
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
+''')
+
+python3 = Tester('python3', '.py',
+'''#!/usr/bin/env python3
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+''',
+'''#!/usr/bin/env python3
 
 def green():
   pass
--- tests/test.py
+++ tests/test.py
@@ -6,16 +6,17 @@ from boil import *
             
 
 class TestBoil(unittest.TestCase):
-    def runCodeTest(self, tester, test_extension=True):
-        with self.subTest('basic {0} language'.format(tester.lang)):
-            self.assertEqual(
-                Boiler().plate(lang=tester.lang),
-                tester.default)
+    def runCodeTest(self, tester, test_language=True, test_extension=True):
+        if test_language:
+            with self.subTest('basic {0} language'.format(tester.lang)):
+                self.assertEqual(
+                    Boiler().plate(lang=tester.lang),
+                    tester.default)
 
-        with self.subTest('advanced {0} language'.format(tester.lang)):
-            self.assertEqual(
-                Boiler().plate(lang=tester.lang, **codeTester.options),
-                tester.advanced)
+            with self.subTest('advanced {0} language'.format(tester.lang)):
+                self.assertEqual(
+                    Boiler().plate(lang=tester.lang, **codeTester.options),
+                    tester.advanced)
 
         if test_extension:
             with self.subTest('basic {0} extension'.format(tester.ext)):
@@ -28,12 +29,23 @@ class TestBoil(unittest.TestCase):
                     Boiler().plate(ext=tester.ext, **codeTester.options),
                     tester.advanced)
 
+    def test_c(self):
+        self.runCodeTest(codeTester.c)
+
+    def test_cpp(self):
+        self.runCodeTest(codeTester.cpp)
 
     def test_java(self):
         self.runCodeTest(codeTester.java)
 
     def test_python(self):
         self.runCodeTest(codeTester.python)
+
+    def test_python2(self):
+        self.runCodeTest(codeTester.python2, test_extension=False)
+
+    def test_python3(self):
+        self.runCodeTest(codeTester.python3, test_extension=False)
 
 if __name__ == '__main__':
     unittest.main()
